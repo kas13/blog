473d8aecff53e30a37a85419a6f806d668661627
kas13 <kasat144@yandex.ru>
2019-01-05 18:15:14 +0300

    add database, login, logout, register

--- blog.py
+++ blog.py
@@ -1,6 +1,10 @@
-from app_dir import app
+from app_dir import app, db
+from app_dir.models import User, Post
 
 
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
 
 if __name__ == "__main__":
